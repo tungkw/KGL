42aad80591cce8cd1f2c9e7d7402c638adb5076f
tungkw <914989053@qq.com>
2021-11-11 02:24:08 +0800

    mvp

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/tungkw/Documents/code/opengl_l/IndexBuffer.cpp" "CMakeFiles/main.dir/IndexBuffer.cpp.obj" "gcc" "CMakeFiles/main.dir/IndexBuffer.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/Logger.cpp" "CMakeFiles/main.dir/Logger.cpp.obj" "gcc" "CMakeFiles/main.dir/Logger.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/Shader.cpp" "CMakeFiles/main.dir/Shader.cpp.obj" "gcc" "CMakeFiles/main.dir/Shader.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/Texture.cpp" "CMakeFiles/main.dir/Texture.cpp.obj" "gcc" "CMakeFiles/main.dir/Texture.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/VertexArray.cpp" "CMakeFiles/main.dir/VertexArray.cpp.obj" "gcc" "CMakeFiles/main.dir/VertexArray.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/VertexBuffer.cpp" "CMakeFiles/main.dir/VertexBuffer.cpp.obj" "gcc" "CMakeFiles/main.dir/VertexBuffer.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/VertexBufferLayout.cpp" "CMakeFiles/main.dir/VertexBufferLayout.cpp.obj" "gcc" "CMakeFiles/main.dir/VertexBufferLayout.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/main.cpp" "CMakeFiles/main.dir/main.cpp.obj" "gcc" "CMakeFiles/main.dir/main.cpp.obj.d"
+  "C:/Users/tungkw/Documents/code/opengl_l/utils/stb_image/stb_image.cpp" "CMakeFiles/main.dir/utils/stb_image/stb_image.cpp.obj" "gcc" "CMakeFiles/main.dir/utils/stb_image/stb_image.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "../libmain.dll.a"
+  "../main.exe"
+  "../main.exe.manifest"
+  "../main.pdb"
+  "CMakeFiles/main.dir/IndexBuffer.cpp.obj"
+  "CMakeFiles/main.dir/IndexBuffer.cpp.obj.d"
+  "CMakeFiles/main.dir/Logger.cpp.obj"
+  "CMakeFiles/main.dir/Logger.cpp.obj.d"
+  "CMakeFiles/main.dir/Shader.cpp.obj"
+  "CMakeFiles/main.dir/Shader.cpp.obj.d"
+  "CMakeFiles/main.dir/Texture.cpp.obj"
+  "CMakeFiles/main.dir/Texture.cpp.obj.d"
+  "CMakeFiles/main.dir/VertexArray.cpp.obj"
+  "CMakeFiles/main.dir/VertexArray.cpp.obj.d"
+  "CMakeFiles/main.dir/VertexBuffer.cpp.obj"
+  "CMakeFiles/main.dir/VertexBuffer.cpp.obj.d"
+  "CMakeFiles/main.dir/VertexBufferLayout.cpp.obj"
+  "CMakeFiles/main.dir/VertexBufferLayout.cpp.obj.d"
+  "CMakeFiles/main.dir/main.cpp.obj"
+  "CMakeFiles/main.dir/main.cpp.obj.d"
+  "CMakeFiles/main.dir/utils/stb_image/stb_image.cpp.obj"
+  "CMakeFiles/main.dir/utils/stb_image/stb_image.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
